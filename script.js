1dee32fa8b01174fd2a56d79862af1fe007822c6
Jake Hall <jakethomashall@gmail.com>
2018-12-23 18:06:28 +0000

    Political parties page progress, reworked cards.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Main Script Document
+
+//"Return to top" button
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
@@ -14,22 +17,41 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+//"Return to top" button end
 
-//Political Parties Page Expand/Close script.
-var idClicked;
-$(document).ready(function() {
-  $("#HiddenElement1").hide();
-  $("a.btn").click(function(e) {
-    idClicked = e.target.id;
-    ExpandShrinkContent();
-  });
+//Political Parties Modals JavaScript
 
-  function ExpandShrinkContent() {
-    if (idClicked == "BtnExpand1") {
-      $("#HideContent1").hide();
-      $("#HideContent2").hide();
-      $("#HiddenElement1").fadeIn(800);
+//Function called from button, element ID is passed into "idClicked" upon button click.
+function ShowModal(idClicked) {
+  //switch statement decides which modal to load based on button ID.
+  switch (idClicked) {
+    case "BtnExpand1":
+      $('#ModalConservatives').modal('show');
       AOS.refresh();
-    }
+      break;
+    case "BtnExpand2":
+      $('#ModalLabour').modal('show');
+      AOS.refresh();
+      break;
+    case "BtnExpand3":
+      $('#ModalLibDem').modal('show');
+      AOS.refresh();
+      break;
+    case "BtnExpand4":
+      $('#ModalUKIP').modal('show');
+      AOS.refresh();
+      break;
+    case "BtnExpand5":
+      $('#ModalGreens').modal('show');
+      AOS.refresh();
+      break;
+    case "BtnExpand6":
+      $('#ModalSNP').modal('show');
+      AOS.refresh();
+      break;
+    default:
+      Console("Unknown case")
   }
-});
+  
+}
+//Political Parties Modals JavaScript end
